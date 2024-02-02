20504f10cba503889d9e05691dcacf0e3f80bbcf
Charles Louisin <clynlouisin@gmail.com>
2024-02-02 20:17:09 +0100

    90%

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-var hotels = ['Serena', 'Falaise', 'Hilton', 'Git']
+// Array data type
+var hotels = ["Serena" , "Falaise" , "Hilton" , "Git"]
+hotels.forEach(
+    function(hotels){
+        console.log(hotels);
+    }
+)
+document.querySelector('.box').innerHTML = "<h1>Hotels proposés :</h1>" + hotels;
 
 
+// Prompt number
+var firstName = prompt("Entre ta note du premier test")
+var lastName = prompt("Entre ta note du deuxieme test")
+function addition() {
+    alert(firstName + lastName)
+}
+addition()
+
+// toggle button and toggleText button
 var hideshow = false;
 
 function addremove(event) {
