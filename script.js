0e8677ebe19dc2b26d85efbb69762cc932fc30eb
Charles Louisin <clynlouisin@gmail.com>
2024-02-06 19:16:50 +0100

    assigment

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-// Array data type
-var hotels = ["Serena" , "Falaise" , "Hilton" , "Git"]
-hotels.forEach(
-    function(hotels){
-        console.log(hotels);
-    }
-)
-document.querySelector('.box').innerHTML = "<h1>Hotels proposés :</h1>" + hotels;
 
-
-// Prompt number
-var firstNumber = prompt("ANCIEN SOLDE")
-var lastNumber = prompt("MONTANT DES ACHATS")
-function soustraction() {
-    alert("VOTRE NOUVEAU SOLDE EST DE :" +" "+ (firstNumber - lastNumber)+"FCFA")
+function image1() {
+    var img = document.querySelector(".change");
+    var text1 = document.querySelector("h1");
+    var text2 = document.querySelector("h4");
+    var boxShadow = document.querySelector('div.box1');
+    img.src = './images/jumpman-mvp/Jordan-MVP-Olive-removebg-preview.png';
+    text1.innerHTML = "New nike <br> Jordan MVP Olive";
+    text2.innerHTML = "This new Nike Air JORDAN MVP OLIVE is the most confortable of all. <br> Suitable for sports and outings.";
+    boxShadow.classList.toggle("boxshadow");
 }
-soustraction()
 
+function image2() {
+    var img = document.querySelector(".change");
+    var text1 = document.querySelector("h1");
+    var text2 = document.querySelector("h4");
+    var boxShadow = document.querySelector('div.box2');
+    img.src = './images/Luka 2/luka1-removebg-preview.png';
+    text1.innerHTML = "New nike <br> Luka 1 Bred";
+    text2.innerHTML = "Over seven years in the making, the Nike Air <br> LUKA 1 BRED places your foot directly <br> on Nike Air technology."
+    boxShadow.classList.toggle("boxshadow");
+}
 
+function image3() {
+    var img = document.querySelector(".change");
+    var text1 = document.querySelector("h1");
+    var text2 = document.querySelector("h4");
+    var boxShadow = document.querySelector('div.box3');
+    img.src = './images/jordan-6-rings/Jordan-Six-Rings-Motorsport-removebg-preview.png';
+    text1.innerHTML = "New nike <br> Jordan 6 Rings";
+    text2.innerHTML = "JORDAN 6 RINGS is the best basketball shoe. <br> Protect your feet and add the trigger. <br> And has a beautiful design";
+    boxShadow.classList.toggle("boxshadow");
+}
 
-
-// toggle button and toggleText button
-var hideshow = false;
-
-function addremove(event) {
-    var text = event.textContent || event.innerText;
-
-    if(!hideshow) {
-        hideshow = true;
-        document.querySelector('.box').style.opacity="100%";
-        event.innerHTML = 'REMOVE';
-        return;
-    }
-
-    if(hideshow) {
-        hideshow = false;
-        document.querySelector('.box').style.opacity="0%";
-        event.innerHTML = 'ADD';
-        return;
-    }   
+function image4() {
+    var img = document.querySelector(".change");
+    var text1 = document.querySelector("h1");
+    var text2 = document.querySelector("h4");
+    var boxShadow = document.querySelector('div.box4');
+    img.src = './images/jordan 4 retro/j_4_retro-removebg-preview.png';
+    text1.innerHTML = "New nike <br> Jordan 4 Retro";
+    text2.innerHTML = "The JORDAN 4 RETRO is one of the best shoes made by Nike. With its perfect design, it is embedded in your sport as in your walks. It's a pass everywhere.";
+    boxShadow.classList.toggle("boxshadow");
 }
